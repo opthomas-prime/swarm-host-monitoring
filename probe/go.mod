dc55766cede9d0f2d84b3f6abb4c6fd31874b9ed
Thomas Maier <contact@thomas-maier.net>
2019-03-28 14:30:12 +0100

    prototype for probe image

--- probe/go.mod
+++ probe/go.mod
@@ -3,7 +3,13 @@ module github.com/opthomas-prime/swarm-host-monitoring/probe
 go 1.12
 
 require (
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/shirou/gopsutil v2.18.12+incompatible
-	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190327214358-63eda1eb0650 // indirect
 	golang.org/x/sys v0.0.0-20190322080309-f49334f85ddc // indirect
 )
--- probe/main.go
+++ probe/main.go
@@ -1,18 +1,74 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	docker "github.com/docker/docker/client"
+
+	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/mem"
 )
 
-func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		v, _ := mem.VirtualMemory()
-		fmt.Fprintln(w, v)
-	})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+func panicOnErr(err error) {
+	if err != nil {
 		panic(err)
+	}
+}
+
+type NodeInfo struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	cli, err := docker.NewEnvClient()
+	panicOnErr(err)
+
+	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		dockerInfo, err := cli.Info(context.Background())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		info := NodeInfo{dockerInfo.Name}
+		bytes, err := json.Marshal(info)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = fmt.Fprintln(w, string(bytes))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/mem", func(w http.ResponseWriter, r *http.Request) {
+		memInfo, err := mem.VirtualMemory()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = fmt.Fprintln(w, memInfo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/load", func(w http.ResponseWriter, r *http.Request) {
+		loadInfo, err := load.Avg()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = fmt.Fprintln(w, loadInfo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	if err := http.ListenAndServe(":8123", nil); err != nil {
+		panicOnErr(err)
 	}
 }
